c8287ba38b18fbfccfeefe094b3a58ba2e2255ba
Jordan <isakaru12@yahoo.com>
2019-04-14 20:01:08 -0400

    Revert "Merge branch 'master' of https://github.com/hsong24/BagelsInTheBreakroom"
    This reverts commit cccdfa9733c38d2e663cfdbbaf8e61807a46a938, reversing
    changes made to 70e7b880ba3fd9dec933136661f57945bec13b2c.

--- Bagels/bagels in the breakroom/Assets/Scripts/NewMove.cs
+++ Bagels/bagels in the breakroom/Assets/Scripts/NewMove.cs
@@ -5,70 +5,44 @@ using UnityEngine.UI;
 
 public class NewMove : MonoBehaviour
 {
-    public float startSpeed = 400f;
+    public float speed = 400f;
+
     public float dashTime = .5f;
     public bool hasBoost; 
     float firstSpeed;
-	private float currentTime = 0f;
+    private float currentTime = 0;
 	public bool isFirstPlace = false;
     public Text playerPlacement;
-	public float rotato = 1.18f;
-	public bool kicking;
-	public Vector3 moveDir;
-	public bool bump = false;
-	public float speed;
-
     // Start is called before the first frame update
     void Start()
     {
         hasBoost = false; //
-		speed = startSpeed; 
-		moveDir = new Vector3(0,0,0);
-		kicking = false;
-		bump = false;
+        firstSpeed = speed; 
     }
 
     // Update is called once per frame
     void FixedUpdate()
-	//so the problem is you can only get currentTime in the right place or kicking in the right place
     {
-        
-		transform.Rotate(0, Input.GetAxis("Horizontal") * rotato, 0);
-		moveDir = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z);
-        if (hasBoost == true)
-        {	
-			if (bump == true) {
-				currentTime = 0;
-				bump = false;
-			}
-			if (kicking == true) {
-				moveDir = new Vector3(moveDir.x, moveDir.y,-moveDir.z);
-				Debug.Log("change dir");
-			}
+        transform.Rotate(0, Input.GetAxis("Horizontal") * 1.18f, 0);
+        Vector3 moveDir = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z);
+        transform.position += moveDir * speed * Time.deltaTime * Input.GetAxis("Vertical");
 
-			if (currentTime == 0f)
+        if (hasBoost == true)
+        {
+            if (currentTime == 0)
             {
                 Debug.Log("Dash");
                 gameObject.GetComponent<Animator>().SetBool("Boost", true);
-				speed = startSpeed * 3;
+                speed = speed * 3;
             }
-
             currentTime += Time.deltaTime;
-
             if (currentTime > dashTime)
             {
-				kicking = false;
-				Debug.Log("end boost");
-				speed = startSpeed;
+                speed = speed / 3f;
                 hasBoost = false;
-				currentTime = 0f;
-				gameObject.GetComponent<Animator>().SetBool("Boost", false);
-				moveDir = new Vector3(moveDir.x, moveDir.y,-moveDir.z);
+                currentTime = 0;
             }
         }
-
-		transform.position += moveDir * speed * Time.deltaTime * Input.GetAxis("Vertical");
-
     }
 
     void Update()
@@ -86,7 +60,7 @@ public class NewMove : MonoBehaviour
     private void BoostAnimationEvent()
     {
         GameObject ethan = gameObject.transform.GetChild(0).gameObject.transform.GetChild(3).gameObject;
-        ethan.GetComponent<Animator>().SetTrigger("Boost");
+        ethan.GetComponent<Animator>().SetBool("Boost", true);
         gameObject.GetComponent<Animator>().SetBool("Boost", false);
 
     }
